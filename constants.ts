210a8eec0d06ad2d2cb1df88b2492e4dcb7c4b5c
Kitsune1904 <annagumiho190423@gmail.com>
2024-11-09 19:15:20 +0200

    add docker

--- constants.ts
+++ constants.ts
@@ -1,13 +1,12 @@
-import path from "path";
 import dotenv from 'dotenv'
 
-const envFile: string = process.env.NODE_ENV === 'production' ? './env/.env.production' : './env/.env.development';
-dotenv.config({ path: envFile });
+if(process.env.NODE_ENV == "local"){
+    const envFile: string = process.env.NODE_ENV === 'production' ? './env/.env.production' : './env/.env.development';
+    dotenv.config({ path: envFile });
+}
 
 export const PORT: number = Number(process.env.PORT) || 5000;
 export const LOG_FILE: string = process.env.LOG_FILE || './logs/productsUpload.dev.log';
-
-export const PRODUCTS_FILE: string = path.join('repository', 'products.store.json');
 
 export const JWT_KEY: string = process.env.JWT_KEY || 'secret'
 
