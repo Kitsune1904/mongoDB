2e1b419044bcc5ec0efbfff35673b6953a654288
Kitsune1904 <annagumiho190423@gmail.com>
2024-11-09 19:35:05 +0200

    changed git ignore

--- constants.ts
+++ constants.ts
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv'
 
 if(process.env.NODE_ENV == "local"){
-    const envFile: string = process.env.NODE_ENV === 'production' ? './env/.env.production' : './env/.env.development';
-    dotenv.config({ path: envFile });
+    dotenv.config({ path: './env/.env.development' });
 }
 
 export const PORT: number = Number(process.env.PORT) || 5000;
@@ -14,4 +13,4 @@ export const JWT_KEY: string = process.env.JWT_KEY || 'secret'
 export const ADMIN_NAME: string = process.env.ADMIN_NAME || 'secret';
 export const ADMIN_EMAIL: string = process.env.ADMIN_EMAIL || 'secret';
 export const ADMIN_PASSWORD: string = process.env.ADMIN_PASSWORD || 'secret';
-export const MONGO: string = process.env.MONGO || 'NAN';
+export const MONGO: string = process.env.MONGO || 'mongodb+srv://annanorri992:zmY3cdivliOZjnKq@cluster0.8e0gg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
